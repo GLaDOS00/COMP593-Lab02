25b795ee616ec89063439cd704c785650868894f
GLaDOS00 <zackary.clarke@flemingcollege.ca>
2022-06-07 15:26:00 -0400

    Rough copy of script

--- process_sales_data.py
+++ process_sales_data.py
@@ -1,6 +1,10 @@
 from sys import argv, exit
 import os
 from datetime import date, datetime 
+import pandas as pd 
+import re
+
+
 
 # Command Line Parameters
 def get_sales_csv():
@@ -38,7 +42,57 @@ def get_order_dir(sales_csv):
 
     return order_dir    
 
+def split_sales_into_orders(sales_csv, order_dir):
+    
+    # Reading data from sales data CSV into DataFrame
+    sales_df = pd.read_csv(sales_csv)
+
+    # Inserting new column for total price of order
+    sales_df.insert(7, 'TOTAL PRICE', sales_df['ITEM QUANTITY'] * sales_df['ITEM PRICE']) 
+
+    # Removing redudant columns from DataFrame
+    sales_df.drop(columns=['ADDRESS', 'CITY', 'STATE','POSTAL CODE', 'COUNTRY'], inplace=True) 
+
+    for order_id, order_df in sales_df.groupby('ORDER ID',):
+
+        # Dropping the ORDER ID column
+        order_df.drop(columns= ['ORDER ID'], inplace=True)
+
+        # Sort the order by order number
+        order_df.sort_values(by='ITEM NUMBER', inplace=True)
+
+        # Adding grand total row at the bottom of column
+        grand_total = order_df['TOTAL PRICE'].sum()
+        grand_total_df = pd.DataFrame({'ITEM PRICE': ['GRAND TOTAL'], 'TOTAL PRICE': [grand_total]})
+        order_df = pd.concat([order_df, grand_total_df]) 
+
+        # Determining the save path of the order file
+        customer_name = order_df['CUSTOMER NAME'].values[0]
+        customer_name = re.sub(r'\W','', customer_name)
+        order_file_name = 'Order' + str(order_id) + '_' + customer_name + '.xlsx'
+        order_file_path = os.path.join(order_dir, order_file_name)
+
+        # Saving the order information to an Excel spreadsheet
+        sheet_name = 'Order #' + str(order_id)
+        writer = pd.ExcelWriter(order_df, engine= 'xlsxwriter')
+        order_df.to_excel(order_file_path, index=False, sheet_name=sheet_name)
+        workbook = writer.book
+        worksheet = writer.sheets['report']
+
+        # Adding a number format for cells with money
+        money_fmt = workbook.add_format({'num_format': '$###,###.##','bold': True})
+
+        # Formatting of column width
+        worksheet.set_column('A', 11)
+        worksheet.set_column('B', 13)
+        worksheet.setcolumn('C:E', 15)
+        worksheet.setcolumn('F:G', 12, money_fmt)
+        worksheet.setcolumn('H', 10)
+        worksheet.setcolumn('I', 30)
+
+
+
 
 sales_csv = get_sales_csv()
 order_dir = get_order_dir(sales_csv)
-
+split_sales_into_orders(sales_csv, order_dir)
